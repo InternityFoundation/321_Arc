f97bc3a98a8587084fcacff55ebaafa77384a085
Anu2199 <32329038+Anu2199@users.noreply.github.com>
2019-11-09 23:55:28 +0530

    Add files via upload

--- oncologer_code.ino
+++ oncologer_code.ino
@@ -1,12 +1,16 @@
 int ir = A0;
 int ir1 = A1;
+float z;
 void setup() {
   // put your setup code here, to run once:
 pinMode(ir,INPUT);
-Serial.begin(115200);
+Serial.begin(9600);
 }
 
 void loop() {
+
+
+  for(int t=0;t<1000;t++){
    float ira = (48.828*(analogRead(ir)))-3;
    float ir1a = (48.828*(analogRead(ir1)))-3;
    float irb = ira/0.03;
@@ -19,8 +23,12 @@ void loop() {
       //Serial.println("ir1a");
    //Serial.println(ir1);
       //Serial.println(ir1b);
-
+  z=z+fin;
+  delay(1);
+  }
+  z=z/1000;
    Serial.println("impedence");
-   Serial.println(fin);
-delay(500);
+   Serial.println(z);
+//delay(500);
+z=0;
 }
